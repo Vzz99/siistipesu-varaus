526e11a26c67aecab81640feb52569fd5f6c8161
Vzz99 <valtterijutila54@gmail.com>
2026-04-23 13:41:53 +0300

    Update AdminPanel.tsx

--- artifacts/window-cleaning/src/components/AdminPanel.tsx
+++ artifacts/window-cleaning/src/components/AdminPanel.tsx
@@ -4,7 +4,7 @@ import { CalendarPicker } from "@/components/CalendarPicker";
 
 const TIME_SLOTS = [
   "08:00", "09:00", "10:00", "11:00", "12:00",
-  "13:00", "14:00", "15:00", "16:00",
+  "13:00", "14:00", "15:00", "16:00", "17:00", "18:00",
 ];
 
 const MONTH_NAMES = [
